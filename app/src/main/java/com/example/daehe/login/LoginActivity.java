1e72348d2566e7b40a71ab8fa2a8281e383a6a31
ChaozFire <Bryandu0ng@yahoo.com>
2018-02-13 12:08:58 -0800

    Revert "Merge branch 'BryanD'"
    This reverts commit 3767521627174a00513030a6368221f11b225a45, reversing
    changes made to 8b3ca3342004cc0d70953ed1749cc0860b5930fe.

--- app/src/main/java/com/example/daehe/login/LoginActivity.java
+++ app/src/main/java/com/example/daehe/login/LoginActivity.java
@@ -5,8 +5,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.facebook.FacebookSdk;
-
 public class LoginActivity extends AppCompatActivity {
 
     @Override
